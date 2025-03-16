70f09addcbb6d0fcf114cce27c51325fc3e2b754
Farrid Kuntoro <50895949+farridkun@users.noreply.github.com>
2025-03-17 00:25:08 +0700

    tracking(UMAMI): [Analytics] : Profile

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,10 @@
 import "./src/styles/global.css"
+import React from "react";
+
+export const onClientEntry = () => {
+  const script = document.createElement("script");
+  script.src = "https://cloud.umami.is/script.js";
+  script.defer = true;
+  script.dataset.websiteId = "3f1f92a1-0041-42ce-add6-c03b50a193ca";
+  document.head.appendChild(script);
+};
